07312be1b99a4420ace9ee0570f6ccaf710df34c
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-03-31 21:27:34 +0300

    add class espresso

---
+++ src/product/Espresso.java
@@ -0,0 +1,8 @@
+package product;
+
+public class Espresso implements MakeCoffee{
+    @Override
+    public void makeCoffee() {
+        System.out.println("Your product.Espresso. Enjoy!");
+    }
+}
--- src/product/MakeCoffee.java
+++ src/product/MakeCoffee.java
@@ -1,3 +1,5 @@
+package product;
+
 public interface MakeCoffee {
     void makeCoffee();
 }
